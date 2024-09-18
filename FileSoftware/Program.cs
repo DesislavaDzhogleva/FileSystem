cc4c076b679c75426d883a308d1fbaea33ecf8cc
Desislava Dzhogleva <desi.dzhogleva@gmail.com>
2024-09-18 20:52:38 +0300

    Added configurations for large files

--- FileSoftware/Program.cs
+++ FileSoftware/Program.cs
@@ -3,8 +3,16 @@ using FileSoftware.Data.Contracts;
 using FileSoftware.Data.Extensions;
 using FileSoftware.Data.Repositories;
 using FileSoftware.Services;
+using Microsoft.AspNetCore.Http.Features;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Limits.MaxRequestBodySize = 5368709120;
+    serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(30);
+    serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(30);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,6 +22,10 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddCustomDbContext(builder.Configuration);
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 5368709120;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
